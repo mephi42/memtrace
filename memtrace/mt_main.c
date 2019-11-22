5591cf5af44efadf2b72160d21528c7c726ea2f6
mephi42 <mephi42@gmail.com>
2019-11-22 12:59:26 +0100

    Instrument Ist_Store

--- memtrace/mt_main.c
+++ memtrace/mt_main.c
@@ -1,7 +1,13 @@
+#include "pub_core_aspacemgr.h"
+#include "pub_core_libcfile.h"
+#include "pub_core_syscall.h"
 #include "pub_tool_basics.h"
 #include "pub_tool_libcassert.h"
+#include "pub_tool_libcfile.h"
 #include "pub_tool_libcprint.h"
 #include "pub_tool_tooliface.h"
+#include "pub_tool_vki.h"
+#include "pub_tool_vkiscnums.h"
 
 #if defined(VG_BIGENDIAN)
 #define END Iend_BE
@@ -25,18 +31,72 @@ typedef UInt UIntPtr;
 #error "Unsupported VG_WORDSIZE"
 #endif
 
-static inline IRExpr* mkUIntPtr(UIntPtr val)
+static IRExpr* mkUIntPtr(UIntPtr val)
 {
    return IRExpr_Const(IRConst_UIntPtr(val));
 }
 
-static inline IRExpr* mkPtr(void* ptr)
+static IRExpr* mkPtr(void* ptr)
 {
    return mkUIntPtr((UIntPtr)ptr);
 }
 
-static ULong count;
-#define Ity_Count Ity_I64
+typedef struct {
+   UIntPtr pc;
+   UIntPtr addr;
+} MTEntry;
+
+#define MAX_TRACE_SIZE (1024 * 1024 * 1024)
+static const char trace_file_name[] = "memtrace.out";
+static int trace_fd;
+static MTEntry* trace_start;
+static MTEntry* trace;
+
+static void add_entry(IRSB* out, Addr pc, IRExpr* addr)
+{
+   IRTemp currentEntryPtr;
+   IRExpr* loadEntryPtr;
+   IRTemp pcPtr;
+   IRExpr* calculatePcPtr;
+   IRTemp addrPtr;
+   IRExpr *calculateAddrPtr;
+   IRTemp updatedEntryPtr;
+   IRExpr* incEntryPtr;
+   /* currentEntryPtr = trace; */
+   currentEntryPtr = newIRTemp(out->tyenv, Ity_Ptr);
+   loadEntryPtr = IRExpr_Load(END, Ity_Ptr, mkPtr(&trace));
+   addStmtToIRSB(out, IRStmt_WrTmp(currentEntryPtr, loadEntryPtr));
+   /* pcPtr = &currentEntryPtr->pc; */
+   pcPtr = newIRTemp(out->tyenv, Ity_Ptr);
+   calculatePcPtr = IRExpr_Binop(Iop_AddPtr,
+                                 IRExpr_RdTmp(currentEntryPtr),
+                                 mkUIntPtr(offsetof(MTEntry, pc)));
+   addStmtToIRSB(out, IRStmt_WrTmp(pcPtr, calculatePcPtr));
+   /* (*pcPtr) = pc; */
+   addStmtToIRSB(out, IRStmt_Store(END,
+                                   IRExpr_RdTmp(pcPtr),
+                                   mkUIntPtr(pc)));
+   /* addrPtr = &currentEntryPtr->addr; */
+   addrPtr = newIRTemp(out->tyenv, Ity_Ptr);
+   calculateAddrPtr = IRExpr_Binop(Iop_AddPtr,
+                                   IRExpr_RdTmp(currentEntryPtr),
+                                   mkUIntPtr(offsetof(MTEntry, addr)));
+   addStmtToIRSB(out, IRStmt_WrTmp(addrPtr, calculateAddrPtr));
+   /* (*addrPtr) = addr; */
+   addStmtToIRSB(out, IRStmt_Store(END,
+                                   IRExpr_RdTmp(addrPtr),
+                                   addr));
+   /* updatedEntryPtr = currentEntryPtr + 1; */
+   updatedEntryPtr = newIRTemp(out->tyenv, Ity_Ptr);
+   incEntryPtr = IRExpr_Binop(Iop_AddPtr,
+                              IRExpr_RdTmp(currentEntryPtr),
+                              mkUIntPtr(sizeof(MTEntry)));
+   addStmtToIRSB(out, IRStmt_WrTmp(updatedEntryPtr, incEntryPtr));
+   /* trace = updatedEntryPtr; */
+   addStmtToIRSB(out, IRStmt_Store(END,
+                                   mkPtr(&trace),
+                                   IRExpr_RdTmp(updatedEntryPtr)));
+}
 
 static void mt_post_clo_init(void)
 {
@@ -51,6 +111,7 @@ IRSB* mt_instrument(VgCallbackClosure* closure,
                     IRType gWordTy,
                     IRType hWordTy)
 {
+   Addr pc = 0;
    IRSB* out;
    int i;
 
@@ -61,22 +122,8 @@ IRSB* mt_instrument(VgCallbackClosure* closure,
          addStmtToIRSB(out, bb->stmts[i]);
          break;
       case Ist_IMark: {
-         IRTemp currentCount;
-         IRExpr* loadCount;
-         IRTemp updatedCount;
-         IRExpr* incCount;
+         pc = bb->stmts[i]->Ist.IMark.addr;
          addStmtToIRSB(out, bb->stmts[i]);
-         currentCount = newIRTemp(out->tyenv, Ity_Count);
-         loadCount = IRExpr_Load(END, Ity_Count, mkPtr(&count));
-         addStmtToIRSB(out, IRStmt_WrTmp(currentCount, loadCount));
-         updatedCount = newIRTemp(out->tyenv, Ity_Count);
-         incCount = IRExpr_Binop(Iop_AddPtr,
-                                 IRExpr_RdTmp(currentCount),
-                                 mkUIntPtr(1));
-         addStmtToIRSB(out, IRStmt_WrTmp(updatedCount, incCount));
-         addStmtToIRSB(out, IRStmt_Store(END,
-                                         mkPtr(&count),
-                                         IRExpr_RdTmp(updatedCount)));
          break;
       }
       case Ist_AbiHint:
@@ -93,6 +140,7 @@ IRSB* mt_instrument(VgCallbackClosure* closure,
          break;
       case Ist_Store:
          addStmtToIRSB(out, bb->stmts[i]);
+         add_entry(out, pc, bb->stmts[i]->Ist.Store.addr);
          break;
       case Ist_LoadG:
          addStmtToIRSB(out, bb->stmts[i]);
@@ -124,11 +172,21 @@ IRSB* mt_instrument(VgCallbackClosure* closure,
 
 static void mt_fini(Int exitcode)
 {
-   VG_(printf)("count = %llu\n", count);
+   ULong trace_size;
+   SysRes o;
+   VG_(am_munmap_valgrind)((Addr)trace_start, MAX_TRACE_SIZE);
+   trace_size = (char*)trace - (char*)trace_start;
+   o = VG_(do_syscall2)(__NR_ftruncate, trace_fd, trace_size);
+   if (sr_isError(o)) {
+      VG_(umsg)("error: can't truncate '%s'\n", trace_file_name);
+      VG_(exit)(1);
+   }
+   VG_(close)(trace_fd);
 }
 
 static void mt_pre_clo_init(void)
 {
+   SysRes o;
    VG_(details_name)("Memory Tracer");
    VG_(details_description)("Valgrind tool for tracing memory accesses");
    VG_(details_copyright_author)(
@@ -137,6 +195,33 @@ static void mt_pre_clo_init(void)
    VG_(basic_tool_funcs)(mt_post_clo_init,
                          mt_instrument,
                          mt_fini);
+   o = VG_(open)(trace_file_name,
+                 VKI_O_CREAT | VKI_O_TRUNC | VKI_O_RDWR,
+                 0644);
+   if (sr_isError(o)) {
+      VG_(umsg)("error: can't open '%s'\n", trace_file_name);
+      VG_(exit)(1);
+   }
+   trace_fd = VG_(safe_fd)(sr_Res(o));
+   if (trace_fd == -1) {
+      VG_(umsg)("error: safe_fd for '%s' failed\n", trace_file_name);
+      VG_(exit)(1);
+   }
+   o = VG_(do_syscall2)(__NR_ftruncate, trace_fd, MAX_TRACE_SIZE);
+   if (sr_isError(o)) {
+      VG_(umsg)("error: can't reserve space for '%s'\n", trace_file_name);
+      VG_(exit)(1);
+   }
+   o = VG_(am_shared_mmap_file_float_valgrind)(MAX_TRACE_SIZE,
+                                               VKI_PROT_READ | VKI_PROT_WRITE,
+                                               trace_fd,
+                                               0);
+   if (sr_isError(o)) {
+      VG_(umsg)("error: can't mmap '%s'\n", trace_file_name);
+      VG_(exit)(1);
+   }
+   trace_start = (MTEntry*)sr_Res(o);
+   trace = trace_start;
 }
 
 VG_DETERMINE_INTERFACE_VERSION(mt_pre_clo_init)
