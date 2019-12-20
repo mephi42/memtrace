a0575a218cac0051d882a63d577a459aeb8df8c1
mephi42 <mephi42@gmail.com>
2019-12-20 09:57:19 +0100

    Add setup.py

---
+++ memtrace/python/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name='memtrace',
+    version='0.0.1',
+    author='mephi42',
+    author_email='mephi42@gmail.com',
+    description='memtrace post-processing scripts',
+    url='https://github.com/mephi42/memtrace',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
